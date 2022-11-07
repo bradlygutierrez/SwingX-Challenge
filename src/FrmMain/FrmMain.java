d95dd530aadf033b95ca558759de4fba6635332b
BradlyGutierrez <bradly.gutierrez14520@est.uca.edu.ni>
2022-11-06 21:04:50 -0600

    second commit

--- src/FrmMain/FrmMain.java
+++ src/FrmMain/FrmMain.java
@@ -172,6 +172,7 @@ public class FrmMain extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         colores.getColors().add(jTextField3.getText());
+        this.jComboBox2.removeAllItems();
         setColorInComboBox();
         
         
@@ -181,10 +182,7 @@ public class FrmMain extends javax.swing.JFrame {
         for(String a: colores.getColors()){
             {
                 this.jComboBox2.addItem(a);
-       
             }
-            
-
         }
     }
     /**
